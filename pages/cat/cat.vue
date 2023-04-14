d613a85e68aef8cc169d11e2b83a7916b3e9293d
richard-home <402822848@qq.com>
2023-04-15 00:13:55 +0800

    tabbar

---
+++ pages/cat/cat.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    我是 cat 页面
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+      <div>我是home页面</div>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
